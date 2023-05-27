a80c3962a48289e537091d00a3d4bc2ae9b25fdd
malakohv <121844616+malakohv@users.noreply.github.com>
2023-05-26 23:16:03 -0400

    Update pmap.sh

--- pmap.sh
+++ pmap.sh
@@ -1,25 +1,16 @@
-#!/usr/bin/sh
-
-echo "Select Target"
-read -p "- " TAR
-TAR=${TAR}
-
-PORT="7070"
-
-while true; do
-
-    Result=$(nmap -p $PORT $TAR)
-
-if [[ $Result == *"$PORT/tcp filtered"* ]]
-
-then  
-    sleep 10
- 
-else 
-    Result=$(nmap -p $PORT $TAR)
-
-if [[ $Result == *"$PORT/tcp open"* ]]; then
-echo "Port is open!"
-break
-
-fi
+TAR=${TAR:-115.187.37.45}
+ 6
+ 7 PORT="7070"
+ 8
+ 9 if [[ $Result == *"$PORT/tcp open"* ]]
+10 echo "PORT IS OPEN"
+11 
+12 else
+13    while [[ $Result == *"$PORT/tcp filtered"* ]]                
+14    do
+15      Result=$(nmap -p $PORT $TAR
+16    done
+17    echo "PORT FILTERED"
+18    sleep 10
+19 fi
+20   
